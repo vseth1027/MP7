aab13be38f5ed6160ec41b4897e59d8f8920c806
Vidushi Seth <vidushiseth@wirelessprv-10-193-73-5.near.illinois.edu>
2017-12-03 15:33:09 -0600

    added getters and setters

--- Warrior.java
+++ Warrior.java
@@ -9,11 +9,43 @@ public class Warrior {
 	
 	public Warrior() {
 		name = "";
-		healthPoints = 0;
+		healthPoints = 100;
 		attackLevel = 0;
 		defenseLevel = 0;
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+
+	public void setHealthPoints(int healthPoints) {
+		this.healthPoints = healthPoints;
+	}
+
+	public int getAttackLevel() {
+		return attackLevel;
+	}
+
+	public void setAttackLevel(int attackLevel) {
+		this.attackLevel = attackLevel;
+	}
+
+	public int getDefenseLevel() {
+		return defenseLevel;
+	}
+
+	public void setDefenseLevel(int defenseLevel) {
+		this.defenseLevel = defenseLevel;
+	}
+
 	public boolean attack(final Warrior opponent) {
 		if (this.defenseLevel > opponent.attackLevel) {
 			this.defenseLevel -= DEFENSE_DAMAGE;
