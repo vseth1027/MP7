c9d8720c4d70056290c21d47e3e7e35a354619d9
Vidushi Seth <vidushiseth@wirelessprv-10-193-73-5.near.illinois.edu>
2017-12-03 14:22:55 -0600

    added Battleground.java to github repository

---
+++ Battleground.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Battleground {
+
+	static final int MAX_HIT_POINTS = 50; 
+	static final int MAX_NUM_ROUNDS = 10;
+	static final int MAX_ATK = 49;
+	
+	/** Minimum attack and defense level; */
+	static final int MIN = 1;
+	
+	static final int MAX_DEF = 23;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * Build a warrior using user input.
+	 * @return the new warrior
+	 */
+	public Warrior buildWarrior() {
+		return null;
+	}
+
+}
