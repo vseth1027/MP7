28957813bfdc2cb51b9c339c564eaa2fbfdfc74b
Vidushi Seth <vidushiseth@wirelessprv-10-193-73-5.near.illinois.edu>
2017-12-03 15:09:52 -0600

    wrote attack method

--- Warrior.java
+++ Warrior.java
@@ -2,18 +2,25 @@
 public class Warrior {
  
 	private String name;
-	private int hitPoints;
+	private int healthPoints;
 	private int attackLevel;
 	private int defenseLevel;
+	static int DEFENSE_DAMAGE = 5;
 	
 	public Warrior() {
 		name = "";
-		hitPoints = 0;
+		healthPoints = 0;
 		attackLevel = 0;
 		defenseLevel = 0;
 	}
 	
 	public boolean attack(final Warrior opponent) {
-		return false; 		// change this
+		if (this.defenseLevel > opponent.attackLevel) {
+			this.defenseLevel -= DEFENSE_DAMAGE;
+			return false;
+		}
+		
+		this.healthPoints -= opponent.attackLevel - this.defenseLevel;
+		return true; 		
 	}
 }
