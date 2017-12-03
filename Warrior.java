7027e9039c69848270847fb76ea7fa7ca6cf544c
Vidushi Seth <vidushiseth@wirelessprv-10-193-73-5.near.illinois.edu>
2017-12-03 14:27:53 -0600

    added Warrior.java to github repository

---
+++ Warrior.java
@@ -0,0 +1,19 @@
+
+public class Warrior {
+ 
+	private String name;
+	private int hitPoints;
+	private int attackLevel;
+	private int defenseLevel;
+	
+	public Warrior() {
+		name = "";
+		hitPoints = 0;
+		attackLevel = 0;
+		defenseLevel = 0;
+	}
+	
+	public boolean attack(final Warrior opponent) {
+		return false; 		// change this
+	}
+}
